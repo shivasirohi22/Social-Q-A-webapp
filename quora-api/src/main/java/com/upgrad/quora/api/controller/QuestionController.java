7468405b35cf05d37111c96b6d5a2a1435150d86
Haider Raza <sayedhaiderr@gmail.com>
2021-01-22 20:40:52 +0530

    Update QuestionController.java

--- quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -1,9 +1,14 @@
 package com.upgrad.quora.api.controller;
 
 
+import com.upgrad.quora.api.model.QuestionRequest;
+import com.upgrad.quora.api.model.QuestionResponse;
+import com.upgrad.quora.service.entity.QuestionEntity;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class QuestionController {
+    @Autowired
+    private QuestionService questionService;
     @RequestMapping(
             method = RequestMethod.POST,
             path = "/question/create",
@@ -24,7 +31,7 @@ public class QuestionController {
         questionEntity.setContent(questionRequest.getContent());
         questionEntity = questionService.createQuestion(questionEntity, authorizationToken);
 
-
+    QuestionResponse questionResponse = new QuestionResponse();
         questionResponse.setId(questionEntity.getUuid().toString());
         questionResponse.setStatus("QUESTION CREATED");
 
