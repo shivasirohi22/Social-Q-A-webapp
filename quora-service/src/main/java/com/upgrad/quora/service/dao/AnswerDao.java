01a9476fe590eae84c2e2dc374c4d97b378d40f4
shivasirohi22 <shivasirohi@gmail.com>
2021-01-23 13:12:57 +0530

    implementation of the endpoint /answer/delete/{answerId}

--- quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
@@ -47,4 +47,19 @@ public class AnswerDao {
         entityManager.merge(answerEntity);
     }
 
+    /**
+     * Deleting an answer by given answerId from the database.
+     * @param answerId Id of the answer whose information is to be fetched.
+     * @return Answer details which is to be deleted if exist in the DB else null.
+     */
+
+    public AnswerEntity deleteAnswer(final String answerId) {
+        AnswerEntity deleteAnswer = getAnswerById(answerId);
+        if (deleteAnswer != null) {
+            entityManager.remove(deleteAnswer);
+        }
+        return deleteAnswer;
+    }
+
+
 }
